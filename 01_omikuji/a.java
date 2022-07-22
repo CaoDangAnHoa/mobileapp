dd94b22c8098b581a7c37307c07855b921b11e6c
CaoDangAnHoa <109338846+CaoDangAnHoa@users.noreply.github.com>
2022-07-22 16:12:37 +0900

    Add files via upload

---
+++ 01_omikuji/a.java
@@ -0,0 +1,16 @@
+import java.util.*;
+interface PP<T>{
+    void print(T obj);
+}
+class Jun30{
+    static void abc(PP p){
+        p.print(d);
+    }
+    static Date d;
+    static int n;
+    public static void main(String[] args){
+        d=new Date();
+        n=123;
+        this.abc(obj->System.out.println(obj));
+    }
+}
